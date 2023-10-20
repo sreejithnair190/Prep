c92dc7f6592135ec5f955699e44e47498780f015
sreejithnair190 <sreejiith190nair@gmail.com>
2023-10-21 00:42:36 +0530

    object cloning

---
+++ src/cloning/Main.java
@@ -0,0 +1,9 @@
+package cloning;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Human sreejith = new Human(23, "Sreejith");
+        Human humanClone = (Human) sreejith.clone();
+        System.out.println(humanClone.toString());
+    }
+}
